7ea679297a9bd2001469db9c8b934190a6f4cf34
Fred Zabulon <fredzabulon133@gmail.com>
2023-05-19 04:50:53 +0300

    Initiated the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Home from "./Pages/Home/Home";
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Services from './Pages/Services/Services';
+import Footer from './Components/Footer/Footer';
+// import Dashboard from './Pages/Dashboards/Main/MainDashBoard';
+// import PublicDashBoard from './Pages/Dashboards/Public/PublicDashBorad';
+// import Auth from './Pages/Dashboards/Auth/Auth';
+// import Owner from './Pages/Dashboards/Owner/Owner';
+// import Patient from './Pages/Dashboards/Patient/Patient';
+// import Admin from './Pages/Dashboards/Admin/Admin';
+// import Hospital from './Pages/Dashboards/Hospital/Hospital';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={ <Home />} />
+        <Route path="/about" element={ <About />} />
+        <Route path="/services" element={<Services />} />    
+      </Routes>
+      <Footer />
+
+    </>
+  )
 }
 
 export default App;
---
+++ src/Components/Info/Info.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Info.css";
+
+const Info = () => {  
+    return (
+        <>
+            <div id="hero" className="d-flex align-items-center">
+                <div className="container">
+                    <h1>Welcome to SMRs</h1>
+                    <h2>The Future of medical records is BLOCKCHAIN</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Info
